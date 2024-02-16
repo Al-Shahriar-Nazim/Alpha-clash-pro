42318f6884f80e61a83b53aafc063827d161e74a
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2024-02-17 03:09:04 +0600

    added js code

--- javascript/alphabet.js
+++ javascript/alphabet.js
@@ -17,10 +17,58 @@ function play(){
   conintueGame()
 }
 
+// module 28 (part 1)
+// call back function
+// function handleKeyBoardButtonPress(){
+//   console.log('Thankyou for today')
+// }
+// document.addEventListener('keyup', handleKeyBoardButtonPress)
+function handleKeyBoardButtonPress(event){
+  const playerPrassed=event.key
+  console.log( 'playerPrassed',event.key)
+
+  // get the excepted to pressed
+  const currentAlphabetElement=document.getElementById('current-alphabets')
+  const currentAlphabet=currentAlphabetElement.innerText;
+  const exceptedAlphabets=currentAlphabet.toLowerCase();
+  console.log(playerPrassed,exceptedAlphabets )
+
+  // cheak match or not
+  if(playerPrassed===exceptedAlphabets){
+    console.log('You get point')
+
+    // update score
+    // step-1: get the current score
+    const currentScoreElement=document.getElementById('current-score')
+ const currentScoreText=currentScoreElement.innerText;
+    const currentScore=parseInt(currentScoreText)
+    // console.log( typeof currentScore)
+    // step-2: reduce the life count
+    const newScore =currentScore+1;
+    // step-3 : Display the update life count
+    currentScoreElement.innerText=newScore;
+
+    removeBackgroundColorById(exceptedAlphabets)
+    conintueGame();
+    // remove backgraound clicked after
+
+  }else{
+    console.log('You lost')
+    // step-1 :get the current life number
+    const currentLifeElement=document.getElementById('current-life');
+    const currentLifeText=currentLifeElement.innerText;
+    const currentLife=parseInt(currentLifeText);
+    // step-2 : Reduce the life count
+    const newlife=currentLife-1;
+    // step-3 : Display the updated life count
+    currentLifeElement.innerText=newlife;
+  }
+}
+document.addEventListener('keyup', handleKeyBoardButtonPress)
 // vedio 7
 function conintueGame(){
 const alphabets =getRandomAlphabet( );
-console.log('Your random alphabets :',alphabets);
+// console.log('Your random alphabets :',alphabets);
 // ---vedio 8----
 //set randomly generated alphabet to the screen (show it)
 const currentAlphabetElements=document.getElementById('current-alphabets')
--- javascript/utilities.js
+++ javascript/utilities.js
@@ -11,12 +11,16 @@ function setAlpabetElementById(elementId){
   const element=document.getElementById(elementId)
   element.classList.add('bg-orange-500')
 }
+function removeBackgroundColorById(elementId){
+  const element=document.getElementById(elementId)
+  element.classList.remove('bg-orange-500')
+}
 // vedio 7
 function getRandomAlphabet(){
   const alphabetSring='abcdefghijklmnopqrstuvwxyz';
   const alphabets=  alphabetSring.split('');
   // (26) ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  console.log(alphabets)
+  // console.log(alphabets)
 // get a random index between (0-25)
 const randomNumber =Math.random()*25;
 const index =Math.round(randomNumber);
